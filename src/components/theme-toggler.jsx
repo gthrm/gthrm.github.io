47e2faa0ffc2409c67ea67661f8a736f1ec00e97
cdROma <38502733+gthrm@users.noreply.github.com>
2023-11-24 22:42:37 +0300

    Update theme-toggler.jsx

--- src/components/theme-toggler.jsx
+++ src/components/theme-toggler.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 
 import { css } from '@emotion/react';
@@ -7,14 +7,37 @@ import sun from '../assets/sun.png';
 import moon from '../assets/moon.png';
 
 export default function ThemeTogglerComponent() {
-  const handleToggleTheme = (e, toggleTheme, theme) => {
+  const handleToggleTheme = useCallback((e, toggleTheme, theme) => {
     // that happens before the theme toggled, so we should invert colors
     // eslint-disable-next-line no-undef
     document
       .querySelector('meta[name="theme-color"]')
       .setAttribute('content', theme === 'dark' ? 'white' : '#2d2d2d');
     toggleTheme(e.target.checked ? 'dark' : 'light');
-  };
+  }, []);
+
+  const icons = useMemo(() => ({
+    checked: (
+      <img
+        alt=""
+        src={moon}
+        width="16"
+        height="16"
+        role="presentation"
+        style={{ pointerEvents: 'none' }}
+      />
+    ),
+    unchecked: (
+      <img
+        alt=""
+        src={sun}
+        width="16"
+        height="16"
+        role="presentation"
+        style={{ pointerEvents: 'none' }}
+      />
+    ),
+  }), []);
   return (
     <div
       css={css`
@@ -22,33 +45,12 @@ export default function ThemeTogglerComponent() {
       `}
     >
       <ThemeToggler>
-        {({ theme, toggleTheme }) => theme && (
-        <Toggle
-          icons={{
-            checked: (
-              <img
-                alt=""
-                src={moon}
-                width="16"
-                height="16"
-                role="presentation"
-                style={{ pointerEvents: 'none' }}
-              />
-            ),
-            unchecked: (
-              <img
-                alt=""
-                src={sun}
-                width="16"
-                height="16"
-                role="presentation"
-                style={{ pointerEvents: 'none' }}
-              />
-            ),
-          }}
-          checked={theme === 'dark'}
-          onChange={(e) => handleToggleTheme(e, toggleTheme, theme)}
-        />
+        {({ theme, toggleTheme }) => (
+          <Toggle
+            icons={icons}
+            checked={theme === 'dark'}
+            onChange={(e) => handleToggleTheme(e, toggleTheme, theme)}
+          />
         )}
       </ThemeToggler>
     </div>
