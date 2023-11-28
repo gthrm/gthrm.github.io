af3c2f7c4f9c0cfc55de3dad243c3ed2c11cbf5a
cdROma <38502733+gthrm@users.noreply.github.com>
2023-11-28 18:58:05 +0300

    fix: page title fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Roman\'s Blog',
+    title: 'cdROma.me',
     url: 'https://cdroma.me/',
-    pageTitle: 'Frontend Engineer, Specialty Coffee Enthusiast, and Creator',
+    pageTitle: 'Roman\'s Blog - Frontend Engineer, Specialty Coffee Enthusiast, and Creator',
     description: 'Join me, Roman, on my journey as a frontend engineer, where I explore the realms of web development, share my passion for specialty coffee, and showcase my creative projects.',
     keywords: "Roman, frontend development, web design, JavaScript, React, Vue, CSS, HTML, programming, coding, software engineering, developer tips, tech blog, coding tutorials, specialty coffee, coffee brewing, coffee culture, personal projects, creative coding, technology insights, web development blog, software development, coding lifestyle, programming insights, IT blog, front-end techniques, modern web technologies, coffee enthusiast, project showcase, technology trends, frontend engineer, web development tips, personal development, technology blog, developer's journey, coding experiences, web innovation, digital creativity",
     links: [
