17fb384b42fcf38547c248b3f2fde4a8b317a346
cdROma <38502733+gthrm@users.noreply.github.com>
2025-08-14 23:11:23 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-const PAGE_TITLE = "Roman's Blog - Frontend Engineer, Specialty Coffee Enthusiast, and Creator";
+const PAGE_TITLE =
+  "Roman's Blog - Frontend Engineer, Specialty Coffee Enthusiast, and Creator";
 
 require('dotenv').config();
 
@@ -13,12 +14,15 @@ module.exports = {
     title: 'cdROma.me',
     siteUrl: 'https://cdroma.me',
     pageTitle: PAGE_TITLE,
-    description: 'Join me, Roman, on my journey as a frontend engineer, where I explore the realms of web development, share my passion for specialty coffee, and showcase my creative projects.',
-    keywords: "Roman, frontend development, web design, JavaScript, React, Vue, CSS, HTML, programming, coding, software engineering, developer tips, tech blog, coding tutorials, specialty coffee, coffee brewing, coffee culture, personal projects, creative coding, technology insights, web development blog, software development, coding lifestyle, programming insights, IT blog, front-end techniques, modern web technologies, coffee enthusiast, project showcase, technology trends, frontend engineer, web development tips, personal development, technology blog, developer's journey, coding experiences, web innovation, digital creativity",
+    description:
+      'Join me, Roman, on my journey as a frontend engineer, where I explore the realms of web development, share my passion for specialty coffee, and showcase my creative projects.',
+    keywords:
+      "Roman, frontend development, web design, JavaScript, React, Vue, CSS, HTML, programming, coding, software engineering, developer tips, tech blog, coding tutorials, specialty coffee, coffee brewing, coffee culture, personal projects, creative coding, technology insights, web development blog, software development, coding lifestyle, programming insights, IT blog, front-end techniques, modern web technologies, coffee enthusiast, project showcase, technology trends, frontend engineer, web development tips, personal development, technology blog, developer's journey, coding experiences, web innovation, digital creativity",
     links: [
       { id: '000', name: 'github', url: 'https://share.cdroma.me/hnXii' },
       { id: '001', name: 'codepen', url: 'https://share.cdroma.me/3CBJx' },
       { id: '002', name: 'producthunt', url: 'https://share.cdroma.me/KBpLg' },
+      { id: '003', name: 'itch.io', url: 'https://share.cdroma.me/NHU0l' },
       { id: '004', name: 'ko-fi', url: 'https://share.cdroma.me/q9SBM' },
     ],
   },
